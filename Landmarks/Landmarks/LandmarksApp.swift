fcef34cec2adb808d084d094eede0dae119d942e
mandos1995 <mandos19950620@gmail.com>
2023-08-01 14:24:38 +0900

    Add: LandMarks 프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김민석 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
