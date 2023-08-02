8981bdaf2970544f4136b51b8e64d628ffa1070a
mandos1995 <mandos19950620@gmail.com>
2023-08-02 10:34:31 +0900

    Feat: Create the Row View

---
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by 김민석 on 2023/08/02.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: LandMark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
