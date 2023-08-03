69c1a054fea7962461d07b18dd7a50ca1831bf73
mandos1995 <mandos19950620@gmail.com>
2023-08-03 10:00:41 +0900

    Feat: Combine the Badge Foreground and Background

---
+++ Landmarks/Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,39 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by 김민석 on 2023/08/03.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var badgeSymbols: some View {
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackGround()
+            
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2, y: (3.0 / 4.0) * geometry.size.height)
+            }
+            
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
