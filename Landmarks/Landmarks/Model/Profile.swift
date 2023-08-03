6f1f9d2d0a98aef8919b331b4002232acebd0937
mandos1995 <mandos19950620@gmail.com>
2023-08-03 14:21:29 +0900

    Feat: Display a User Profile

---
+++ Landmarks/Landmarks/Model/Profile.swift
@@ -0,0 +1,25 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by 김민석 on 2023/08/03.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        var id: String { rawValue }
+    }
+}
