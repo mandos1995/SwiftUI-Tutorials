d62278c82055400b0683217f19fd92874b19b54b
mandos1995 <mandos19950620@gmail.com>
2023-08-04 11:11:19 +0900

    Feat: Add a watchOS Target

---
+++ Landmarks/WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by 김민석 on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
